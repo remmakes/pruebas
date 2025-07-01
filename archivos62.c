2fba9f1722ecf37e20c8d98df793db705b681bae
remmakes <remmakes@gmail.com>
2025-07-01 08:52:53 -0300

    agregando archivo

---
+++ archivos62.c
@@ -0,0 +1,10 @@
+/*6.2 Tomando como base el archivo ALUMNOS.DAT creado en el ejercicio anterior se desea desarrollar un
+programa que, basado en la información del archivo, genere 3 archivos:
+• Uno con los alumnos PROMOCIONADOS (ambas notas mayores o igual a 7).
+• Otro archivo con los alumnos en condición CURSADO (ambas notas mayores o igual a 4 pero no
+promocionados).
+• Y un tercer archivo con los REPROBADOS (al menos una nota, menor que 4).
+Para comprobar los datos guardados realizar una función que reciba por parámetro el nombre del archivo
+a mostrar, lo abre y muestre su contenido. Se debe invocar a la función 3 veces para mostrar todos los
+archivos generados.*/
+
